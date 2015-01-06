30aae6a17498698205d8793a8b0551a21efde608
Spencer Eldred <eldredspencer@gmail.com>
2015-01-06 23:29:33 +0000

    Add component hello-world-canvan

---
+++ ui/hello-world-canvan.reel/hello-world-canvan.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/hello-world-canvan.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class HelloWorldCanvan
+ * @extends Component
+ */
+exports.HelloWorldCanvan = Component.specialize(/** @lends HelloWorldCanvan# */ {
+    constructor: {
+        value: function HelloWorldCanvan() {
+            this.super();
+        }
+    }
+});
